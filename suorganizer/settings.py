d3b7fd22dd302e6fbe5ad6413c0482a8745e9be6
Fatunbi David Oluwakayode <fatunbidavidkayode@gmail.com>
2020-07-10 23:41:04 +0100

    Revert "Update settings.py"
    This reverts commit c2b1fe91

--- suorganizer/settings.py
+++ suorganizer/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'u-^+-h$j^hx-0y)zn=gnss0vrf8in+0e=b*asz3t%@2#(^_op#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
